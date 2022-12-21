a43d869e1374f8e0c8bfec077758816a044dc8fa
Vojtech Machacek <vmachacek@gmail.com>
2022-12-21 15:15:03 -0500

    wip

---
+++ ConsoleApp65/Yolo.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp65;
+
+public class Yolo
+{
+    public string[] Foo()
+    {
+        var f = new[]
+        {
+            "afkasj;fklasfjlas;gjklas;fglas;fjgasl;fjasdlf;jasdlf;asdjfl;asdjflasdkfjasl;df",
+            "afkasj;fklasfjlas;gjklas;fglas;fjgasl;fjasdlf;jasdlf;asdjfl;asdjflasdkfjasl;df",
+            "afkasj;fklasfjlas;gjklas;fglas;fjgasl;fjasdlf;jasdlf;asdjfl;asdjflasdkfjasl;df",
+            "afkasj;fklasfjlas;gjklas;fglas;fjgasl;fjasdlf;jasdlf;asdjfl;asdjflasdkfjasl;df",
+            "afkasj;fklasfjlas;gjklas;fglas;fjgasl;fjasdlf;jasdlf;asdjfl;asdjflasdkfjasl;df",
+        };
+
+        return new[] { "Pro", "Foo", };
+    }
+}
